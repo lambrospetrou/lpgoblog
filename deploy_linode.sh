fb7608fb5c2f7aee936518d5e8d11f619a0320d0
Lambros Petrou <lambrospetrou@gmail.com>
2014-08-21 00:12:56 +0100

    added deployment script

--- deploy_linode.sh
+++ deploy_linode.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # check for updates in the packages and dependencies and download them
-go get -u
+#go get -u
+go get
 # build the new Spito
 go build -o goblog-t
 
 # move the executable to the public directory while restarting supervisor
 mv goblog-t /home/lambros/public/lambrospetrou.com/public/blog/goblog
+cp -rf templates /home/lambros/public/lambrospetrou.com/public/blog/templates
+cp -rf static /home/lambros/public/lambrospetrou.com/public/blog/static
+
 sudo supervisorctl restart lpgoblog
